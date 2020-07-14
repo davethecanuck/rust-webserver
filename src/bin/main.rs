a73f92e44897a2a67bbfb38b0d7a74eae031d57a
David Sutherland <davethecanuck@gmail.com>
2020-07-13 21:19:15 -0600

    Use regex to match GET requests

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,9 +4,8 @@ use std::str;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::time::Duration;
-use std::thread;
 use std::io;
+use regex::bytes::Regex;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -17,8 +16,8 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         pool.execute(|| {
-            match handle_connection(stream) {
-                Err(e) => println!("Failed to handle_connection: {:?}", e),
+            match handle_stream(stream) {
+                Err(e) => println!("Failed to handle_stream: {:?}", e),
                 _ => (),
             }
         });
@@ -27,7 +26,7 @@ fn main() {
 
 type ConnHandlerResult = Result<(), io::Error>;
 
-fn handle_connection(stream: TcpStream) -> ConnHandlerResult {
+fn handle_stream(stream: TcpStream) -> ConnHandlerResult {
     let mut conn_handler = ConnHandler::new(stream);
     conn_handler.process()?;
     Ok(())
@@ -35,45 +34,58 @@ fn handle_connection(stream: TcpStream) -> ConnHandlerResult {
 
 struct ConnHandler {
     stream: TcpStream,
-    buffer: [u8; 1024],
+    buffer: [u8; 4096],
+    header_regex: Regex,
 }
 
 impl ConnHandler {
+    fn new(stream: TcpStream) -> ConnHandler {
+        ConnHandler{
+            stream,
+            buffer: [0_u8; 4096],
+            header_regex: Regex::new(r"GET (\S+?)\s+").unwrap(),
+        }
+    }
+
+    fn get_request(&self) -> Option<String> {
+        match self.header_regex.captures(&self.buffer) {
+            Some(cap) => {
+                match String::from_utf8(cap[1].to_vec()) {
+                    Ok(s) => Some(s),
+                    _ => None,
+                }
+            }
+            _ => None
+        }
+    }
+
     // Possibly do all error handling here so we can
     // send appropriate response to client for some errors
     fn process(&mut self) -> ConnHandlerResult {
         self.stream.read(&mut self.buffer)?;
-        println!("Read buffer: \n{}", String::from_utf8_lossy(&self.buffer));
-
-        let get = b"GET / HTTP/1.1\r\n";
-        let sleep = b"GET /sleep HTTP/1.1\r\n";
-        thread::sleep(Duration::from_secs(1));
-    
-        let (status_line, file) = if self.buffer.starts_with(get) {
-            ("200 OK", "htdocs/hello.html")
+        //println!("Read buffer: \n{}", String::from_utf8_lossy(&self.buffer));
+        match self.get_request() {
+            Some(req) => {
+                println!("Request is for {:?}", req);
+                let (status_line, file) = match req.as_str() {
+                    "/" => ("200 OK", "htdocs/hello.html"),
+                    "/sleep" => ("200 OK", "htdocs/sleep.html"),
+                    _ => ("404 NOT FOUND", "htdocs/404.html")
+                };
+                self.send_response(&status_line, &file)?;
+            },
+            None => {
+                println!("Invalid request: \n{}", 
+                    String::from_utf8_lossy(&self.buffer));
+            }
         }
-        else if self.buffer.starts_with(sleep) {
-            thread::sleep(Duration::from_secs(8));
-            ("200 OK", "htdocs/sleep.html")
-        } 
-        else {
-            ("404 NOT FOUND", "htdocs/404.html")
-        };
-        self.send_response(&status_line, &file)?;
         Ok(())
-    }
-
-    fn new(stream: TcpStream) -> ConnHandler {
-        ConnHandler{ 
-            stream,
-            buffer: [0_u8; 1024],
-        }
     }
 
     fn send_response(&mut self, status_line: &str, file: &str) -> ConnHandlerResult {
         let contents = fs::read_to_string(file)?;
         let response = to_http(status_line, contents);
-        println!("Sending response: \n{}", 
+        println!("Sending response: \n{}",
              String::from_utf8_lossy(response.as_bytes()));
         self.stream.write(response.as_bytes()).unwrap();
         self.stream.flush().unwrap();
